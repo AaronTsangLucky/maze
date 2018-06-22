a5e2bbba2d80929e7007d724299439353b30bfe5
aarontsang <aarontsanglucky@163.com>
2018-06-22 23:21:45 +0800

    test

--- maze-core/src/main/java/com/concept/maze/test/thread/pc/ProducerConsumerWithWaitNofity.java
+++ maze-core/src/main/java/com/concept/maze/test/thread/pc/ProducerConsumerWithWaitNofity.java
@@ -5,20 +5,20 @@ public class ProducerConsumerWithWaitNofity {
     public static void main(String[] args) {
         Resource resource = new Resource();
         ProducerThread p1 = new ProducerThread("生产者No1",resource);
-//        ProducerThread p2 = new ProducerThread("生产者No2",resource);
-//        ProducerThread p3 = new ProducerThread("生产者No3",resource);
+        ProducerThread p2 = new ProducerThread("生产者No2",resource);
+        ProducerThread p3 = new ProducerThread("生产者No3",resource);
 
         ConsumerThread c1 = new ConsumerThread("消费者No1", resource);
-//        ConsumerThread c2 = new ConsumerThread("消费者No2", resource);
-//        ConsumerThread c3 = new ConsumerThread("消费者No3", resource);
+        ConsumerThread c2 = new ConsumerThread("消费者No2", resource);
+        ConsumerThread c3 = new ConsumerThread("消费者No3", resource);
 
         p1.start();
-//        p2.start();
-//        p3.start();
+        p2.start();
+        p3.start();
 
         c1.start();
-//        c2.start();
-//        c3.start();
+        c2.start();
+        c3.start();
     }
 
 }
